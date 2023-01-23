59e6cb1d1429108976de3496352c02c53dcf5817
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-23 20:35:15 +0100

    task 1-4

--- 3. React/Homework React/src/App.jsx
+++ 3. React/Homework React/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
-  return <div className="App">Hejo</div>;
+  return (
+    <div className="App">
+      <ShoppingList></ShoppingList>
+    </div>
+  );
 }
 
 export default App;
---
+++ 3. React/Homework React/src/components/ShoppingItem.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+// function ShoppingItem(props) {
+//   return <div>{props.products}</div>;
+// }
+
+// export default ShoppingItem;
