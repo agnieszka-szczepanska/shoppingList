f29002843bb6b1109d6fe3708266ef5e8d77ba03
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-24 11:48:22 +0100

    all tasks complited

--- 3. React/Homework React/src/App.jsx
+++ 3. React/Homework React/src/App.jsx
@@ -1,10 +1,60 @@
 import "./App.css";
 import ShoppingList from "./components/ShoppingList";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setProducts([
+      ...products,
+      { name: inputValue, id: uuidv4(), isCompleted: false },
+    ]);
+  };
+
+  const removeTask = (id) => {
+    setProducts(products.filter((product) => product.id !== id));
+  };
+
+  const scratchProduct = (product) => {
+    product.isCompleted = !product.isCompleted;
+    setProducts([...products]);
+  };
+
   return (
     <div className="App">
-      <ShoppingList></ShoppingList>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Add product"
+          value={inputValue}
+        />
+        <button onClick={handleSubmit}>Add product</button>
+      </form>
+
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <input
+              type="checkbox"
+              checked={product.isCompleted}
+              onChange={() => scratchProduct(product)}
+            />
+            <button onClick={() => removeTask(product.id)}>Delete</button>
+            <div
+              style={{
+                textDecoration: product.isCompleted ? "line-through" : "none",
+              }}
+            >
+              {product.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- 3. React/Homework React/src/components/shoppinglist.jsx
+++ 3. React/Homework React/src/components/shoppinglist.jsx
@@ -1,51 +1,8 @@
 import React from "react";
 // import ShoppingItem from "./ShoppingItem";
-import { useState } from "react";
 
 function ShoppingList() {
-  const [products, setProducts] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setProducts([...products, { product: inputValue }]);
-  };
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add product"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button>Add product</button>
-        {/* <p>{inputValue}</p> */}
-        {/* <p>{products}</p> */}
-
-        {/* <ShoppingItem products={products}></ShoppingItem> */}
-      </form>
-
-      {products.map((product) => (
-        <form>
-          <input
-            type="checkbox"
-            key="product.product"
-            name={product.product}
-            id={products.indexOf(product)}
-          ></input>
-          <label htmlFor="{product.product}">
-            {" "}
-            {product.product}
-            {/* UDAŁO SIE DOTRZEC DO INDEXU- umieścić w ID -*/}
-            {/* {products.indexOf(product)} */}
-          </label>
-          <button>Delete</button>
-        </form>
-      ))}
-    </>
-  );
+  return;
 }
 
 export default ShoppingList;
