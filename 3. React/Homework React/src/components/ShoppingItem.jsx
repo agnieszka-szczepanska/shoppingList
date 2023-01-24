4302a3ed06c3087c8b402ac81d490f1b8c789402
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-24 12:50:22 +0100

    add components

--- 3. React/Homework React/src/components/ShoppingItem.jsx
+++ 3. React/Homework React/src/components/ShoppingItem.jsx
@@ -1,7 +1,39 @@
-// import React from "react";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import ShoppingList from "./ShoppingList";
+import { useState } from "react";
 
-// function ShoppingItem(props) {
-//   return <div>{props.products}</div>;
-// }
+function ShoppingItem() {
+  const [products, setProducts] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-// export default ShoppingItem;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setProducts([
+      ...products,
+      { name: inputValue, id: uuidv4(), isCompleted: false },
+    ]);
+  };
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Add product"
+          value={inputValue}
+        />
+        <button onClick={handleSubmit}>Add product</button>
+      </form>
+      <ShoppingList
+        setInputValue={setInputValue}
+        inputValue={inputValue}
+        products={products}
+        setProducts={setProducts}
+      ></ShoppingList>
+    </>
+  );
+}
+
+export default ShoppingItem;
--- 3. React/Homework React/src/components/shoppinglist.jsx
+++ 3. React/Homework React/src/components/shoppinglist.jsx
@@ -1,8 +1,36 @@
 import React from "react";
-// import ShoppingItem from "./ShoppingItem";
 
-function ShoppingList() {
-  return;
+function ShoppingList(props) {
+  const removeTask = (id) => {
+    props.setProducts(props.products.filter((product) => product.id !== id));
+  };
+
+  const scratchProduct = (product) => {
+    product.isCompleted = !product.isCompleted;
+    props.setProducts([...props.products]);
+  };
+
+  return (
+    <div>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <input
+            type="checkbox"
+            checked={product.isCompleted}
+            onChange={() => scratchProduct(product)}
+          />
+          <button onClick={() => removeTask(product.id)}>Delete</button>
+          <div
+            style={{
+              textDecoration: product.isCompleted ? "line-through" : "none",
+            }}
+          >
+            {product.name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ShoppingList;
